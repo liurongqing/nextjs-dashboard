3d6e11f820aa74b37fb1c976132bb4747305dd4b
liurongqing <phpgege@gmail.com>
2023-11-11 15:25:50 +0800

    1

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,6 @@
 import Pagination from '@/app/ui/invoices/pagination';
+import { Metadata } from 'next';
+
 import Search from '@/app/ui/search';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import Table from '@/app/ui/invoices/table';
@@ -6,7 +8,9 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
- 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page({
   searchParams,
 }: {
